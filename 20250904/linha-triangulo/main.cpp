f19b5ed802b46f4f276422a395beefe303eb0933
Vinicius Pinheiro <73889866+Vinicius-pinh@users.noreply.github.com>
2025-09-04 20:29:25 -0300

    update

--- 20250904/linha-triangulo/main.cpp
+++ 20250904/linha-triangulo/main.cpp
@@ -9,6 +9,30 @@
 static void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    //define a area de enquadramento da cena
+    /*
+         +inf y
+          |
+          |
+-inf______|_______ +infx
+          |
+          |
+          |
+         -inf y
+    */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluOrtho2D(-3, 3, -3, 3);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glLineWidth(10);
+    glBegin(GL_LINES);
+    glVertex2f(-2.0f, 0.0f);
+    glVertex2f(2.0f, 0.0f);
+    glEnd();
+    glFlush();
 }
 
 int main(int argc, char *argv[])
@@ -18,6 +42,7 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutCreateWindow("GLUT Shapes");
+    glClearColor(0, 0, 1, 0);
     glutDisplayFunc(display);
     glutMainLoop();
     return EXIT_SUCCESS;
