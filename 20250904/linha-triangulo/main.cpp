61b19f1d9071c348fac0ed39e0b9a7c3451bcfcf
Vinicius Pinheiro <73889866+Vinicius-pinh@users.noreply.github.com>
2025-09-11 20:29:57 -0300

    commit keyboard iteraction

--- 20250904/linha-triangulo/main.cpp
+++ 20250904/linha-triangulo/main.cpp
@@ -5,7 +5,20 @@
 
 #include <GL/glut.h>
 #include <stdlib.h>
+GLfloat escala=1.0f;
 
+void listeningKey(unsigned char tecla, GLint col, GLint linha){
+    switch(tecla){
+        case '+':
+            escala = escala+0.05f; //escala ++;
+        break;
+        case '-':
+            escala = escala-0.05f;//escala --;
+        break;
+
+    }
+    glutPostRedisplay();
+}
 static void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -25,13 +38,14 @@ static void display(void)
     glLoadIdentity();
 
     gluOrtho2D(-3, 3, -3, 3);
+    glScalef(escala, escala, 0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glLineWidth(10);
-    glBegin(GL_LINES);
+    /*glBegin(GL_LINES);
     glVertex2f(-2.0f, 0.0f);
     glVertex2f(2.0f, 0.0f);
-    glEnd();
+    glEnd();*/
     glBegin(GL_TRIANGLES);
     glColor3f(0, 1, 0);
     glVertex2f(-2.0f, -2.0f);
@@ -49,6 +63,7 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutCreateWindow("GLUT Shapes");
+    glutKeyboardFunc(listeningKey);
     glClearColor(0, 0, 1, 0);
     glutDisplayFunc(display);
     glutMainLoop();
